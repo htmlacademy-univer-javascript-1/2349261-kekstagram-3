183de35e629c2812df4b5cb2504e9ca8dbfc5e31
daniil <dankras13@mail.ru>
2023-03-13 15:08:06 +0300

    module2-task1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 function random(min, max) {
   if (min <= max) {
-  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  else console.log("Min должен быть меньше max");
+  else {
+    return -1;
+  };
 }
 random(0, 112);
 
 function numberOfChars(str, maxLength) {
   if (str.length <= maxLength) {
-    console.log(str.length);
     return true;
   }
-  else return false
+  else
+  {
+    return false;
+  };
 }
 
-numberOfChars("Я люлбю jsasdasddas", 20);
+numberOfChars('Я люлбю jsasdasddas', 20);
