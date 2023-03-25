b9139bd67f9c40390981adaceae1b5b24ac3fde2
daniil <dankras13@mail.ru>
2023-03-26 02:14:44 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -24,15 +24,15 @@ numberOfChars('Я люлбю jsasdasddas', 20);
 function arrayOfPhotos() {
   const resultArray = new Array(25);
   for (let i = 0; i < resultArray.length; i++) {
-      resultArray[i] = (
-        {
-            id: i + 1,
-            url: `photos/${i+1}.png`,
-            description:'море',
-            likes: random(15,200),
-            comments: random(0, 200)
-        }
-      );
+    resultArray[i] = (
+      {
+        id: i + 1,
+        url: `photos/${i+1}.png`,
+        description:'море',
+        likes: random(15,200),
+        comments: random(0, 200)
+      }
+    );
   }
   return resultArray;
 }
