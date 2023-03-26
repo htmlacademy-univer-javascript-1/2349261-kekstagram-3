06fffb19768596890dd5ed38a53c2469ed26be77
daniil <dankras13@mail.ru>
2023-03-26 17:18:17 +0300

    module5-task1

---
+++ js/logic.js
@@ -0,0 +1,18 @@
+function random(min, max) {
+  if (min <= max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  else {
+    return -1;
+  }
+}
+
+function numberOfChars(str, maxLength) {
+  if (str.length <= maxLength) {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
